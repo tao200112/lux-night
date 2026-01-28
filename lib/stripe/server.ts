ff0f0913fbfa9dea3152121f36bdd3161fd58d3f
tao200112 <tao200112@users.noreply.github.com>
2026-01-27 19:40:15 -0500

    fix: update Stripe API version in lib/stripe/server.ts

--- lib/stripe/server.ts
+++ lib/stripe/server.ts
@@ -7,7 +7,7 @@ import Stripe from 'stripe';
  */
 export const stripe: Stripe | null = process.env.STRIPE_SECRET_KEY
   ? new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2024-12-18.acacia',
+      apiVersion: '2025-02-24.acacia',
       typescript: true,
     })
   : null;
